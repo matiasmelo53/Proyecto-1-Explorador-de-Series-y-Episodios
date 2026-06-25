2d9690875ae4096b9fc64115d34d85e29b88a2aa
matiasmelo53 <matiasmelo3600@gmail.com>
2026-06-25 11:05:11 -0400

    Agregado de script base js. Funcionalidad on hover para shows

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+for (let i = 1; i <= 7; i++) {
+    fetch("https://api.tvmaze.com/shows/"+i)
+        //.then(res => console.log(result))
+        .then(response => response.json())
+        .then(res => {
+            let image = document.getElementById("indexRecomendado");
+
+            image.innerHTML += `
+            <td style=" align-items: center; padding: 10px;"><a href="shows.html"><img class="show" src="${res.image.medium}" alt="${res.name}"></a></td>`;
+        })
+        .catch(error => {
+            console.log(error);
+            let recomendado = document.getElementById("indexRecomendado");
+            recomendado.innerHTML += `<td style=" align-items: center; padding: 10px;" ><a href="shows.html">Hubo un problema al cargar la serie.   Index=${i}. Error: ${error}</td>`;
+        });
+}
